b3598b468f294dd2e22088ae94b061ce442b19b4
mokshupadhyaygrip <mokshupadhyay4@gmail.com>
2025-07-03 17:31:28 +0530

    app build size optimized

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,21 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    minifierConfig: {
+      // Remove console.log statements in production
+      mangle: {
+        keep_fnames: true,
+      },
+      output: {
+        comments: false,
+      },
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
